2e90d2d2f3a0074b801c3911d260108f57979b54
Rey9530 <56655911+Rey9530@users.noreply.github.com>
2021-06-19 13:52:48 -0600

    Swiper Tarjetas

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Peliculas en Cine"),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            _swiperTarjetas(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _swiperTarjetas() {
+    return Container(
+      width: double.infinity,
+      height: 300.00,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return new Image.network(
+            "http://via.placeholder.com/350x150",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        itemWidth: 200.0,
+        //pagination: new SwiperPagination(),
+        //control: new SwiperControl(),
+        layout: SwiperLayout.STACK,
+      ),
+    );
+  }
+}
