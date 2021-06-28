38aeb9a473d020dcc30f30d24adfcf2f86e60e50
Rey9530 <alex.rey.rosales.95@gmail.com>
2021-06-27 23:43:07 -0600

    fix bug

--- lib/src/models/pelicula_model.dart
+++ lib/src/models/pelicula_model.dart
@@ -74,7 +74,7 @@ class Pelicula {
   }
 
   getBackgroudImg() {
-    if (posterPath == null) {
+    if (backdropPath == null) {
       return 'https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg';
     } else {
       return 'https://image.tmdb.org/t/p/w500/$backdropPath';
